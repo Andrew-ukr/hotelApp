e35a335895bd1718f3e7079238374a49731ea15c
apashulia <apashulia@binariks.com>
2023-08-05 19:46:24 +0300

    added JWT.sign method and cookie parser

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,39 @@
 import { User } from "../models/userModel.js";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
 export const register = async (req, res) => {
   const { name, email, password, role } = req.body;
   const user = await User.create({ name, email, password, role });
-  return res.status(StatusCodes.OK).json({ success: true, user });
+  const token = jwt.sign({ name, email }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  
+  res.cookie("jwtToken", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    expires: new Date(Date.now() + 3600000),
+  });
+
+  return res.status(StatusCodes.OK).json({ success: true, user, token });
 };
 
 export const login = async (req, res) => {
   const { name, password, email } = req.body;
 
   const user = await User.findOne({ email, name });
-  const isPassword = user && await user.comparePasswords(password)
+  const isPassword = user && (await user.comparePasswords(password));
+
 
   if (user && isPassword) {
     return res.status(200).json("login user");
   } else {
-    return res.status(StatusCodes.UNAUTHORIZED).json({ success: false, message: ReasonPhrases.UNAUTHORIZED });
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ success: false, message: ReasonPhrases.UNAUTHORIZED });
   }
 };
 
