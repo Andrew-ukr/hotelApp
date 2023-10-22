e469b10f54387974d5cc258e7c2105c9b2ac0b5b
apashulia <apashulia@binariks.com>
2023-10-22 19:46:28 +0300

    created UserDropdown menu

--- client/src/Components/HeaderPanel/HeaderPanel.tsx
+++ client/src/Components/HeaderPanel/HeaderPanel.tsx
@@ -1,28 +1,11 @@
-import React from "react";
-import { Button, Search } from "../UI";
-import { useLazyLogoutQuery } from "../../Redux/Slices/authApi";
-import { useDispatch } from "react-redux";
-import { setUser } from "../../Redux/Slices/authSlice";
-import { toast } from "react-toastify";
+import { Search } from "../UI";
+import UserDropdown from "../UserDropdown/UserDropdown";
 
 const HeaderPanel = () => {
-  const [logout, { isLoading }] = useLazyLogoutQuery();
-  const dispatch = useDispatch();
-  const handleLogout = async () => {
-    try {
-      const res = await logout().unwrap();
-    } catch (error) {
-      console.log(error);
-    } finally {
-      dispatch(setUser(null));
-      toast.success("User Logged out");
-    }
-  };
-
   return (
     <div className="flex justify-between py-7">
       <Search />
-      <Button onClick={handleLogout}>Log out</Button>
+      <UserDropdown />
     </div>
   );
 };
